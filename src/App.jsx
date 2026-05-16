ef4d15fa5f6523ab7a39cbdcf5fb43163e55c924
Marco Guevara <marcojgh352@gmail.com>
2026-05-16 19:33:21 +0200

    feat: add protected product catalog

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AuthGateway from './pages/AuthGateway.jsx'
 import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
 import RegisterPage from './pages/RegisterPage.jsx'
+import ShopPage from './pages/ShopPage.jsx'
 import ProtectedRoute from './routes/ProtectedRoute.jsx'
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path="/register" element={<RegisterPage />} />
       <Route element={<ProtectedRoute />}>
         <Route path="/home" element={<HomePage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Route>
     </Routes>
   )
